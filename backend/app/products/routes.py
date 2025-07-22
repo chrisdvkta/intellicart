19c78de779ecf253b4af355b9e7282eba35f4dd8
chrisdvkta <chrisdevkota77@gmail.com>
2025-07-22 10:14:02 +0545

    update: product update

--- backend/app/products/routes.py
+++ backend/app/products/routes.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Body, Depends
 from fastapi.encoders import jsonable_encoder
+from requests import session
 
 from app.models.product import Product, ProductCreate
 from app.products.service import ProductService
@@ -29,6 +30,16 @@ async def post_products(
 @product_router.get("/products/{product_id}")
 async def fetch_product(product_id: int, session: sessionInstance):
     return await product_service.fetch_product(product_id, session)
+
+
+@product_router.put("/products/{product_id}")
+async def update_product(
+    product_id: int,
+    product: Annotated[ProductCreate, Body()],
+    token: Annotated[str, Depends(get_current_token)],
+    session: sessionInstance,
+):
+    return await product_service.update_product(product_id, product, session)
 
 
 # @product_router.put("/product/{id}")
